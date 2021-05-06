0cd0cac8f6ab460b3cb24cf76822fc24d585a478
henryhe1234 <henryhe1123@gmail.com>
2021-05-05 18:02:53 -0600

    basic

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -1,11 +1,23 @@
-class Vehicle{
-  drive():void{
-    console.log('bala bala')
-  }
-  honk():void{
-    console.log('beep')
+class Vehicle {
+  constructor(public color: string) {}
+
+  protected honk(): void {
+    console.log("beep");
   }
 }
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log("cdcd");
+  }
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+const vehicle = new Vehicle("red");
+console.log(vehicle.color);
+const carOne = new Car(1, "orange");
+carOne.startDrivingProcess();
--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,47 +1,45 @@
-let apples : number = 5;
-let info : string = "fast";
-let isThere : boolean = false;
-let empty : null = null;
-let nothing : undefined = undefined 
+let apples: number = 5;
+let info: string = "fast";
+let isThere: boolean = false;
+let empty: null = null;
+let nothing: undefined = undefined;
 //build in object
 
-let time : Date = new Date();
+let time: Date = new Date();
 
-let stringArray : string[] = ["1","b"];
-let numArray: number[] = [3,6,5];
-class Car{}
+let stringArray: string[] = ["1", "b"];
+let numArray: number[] = [3, 6, 5];
 
-let car:Car = new Car();
-let point : {x:number;y:number} = {
-  x:444,
-  y:333
-}
-const logNumber: (j:number) => void = (j:number)=>{
-  console.log(j)
-}
+let point: { x: number; y: number } = {
+  x: 444,
+  y: 333,
+};
+const logNumber: (j: number) => void = (j: number) => {
+  console.log(j);
+};
 //when to use annotations
 //1) Function that return 'any' types
 const json = '{"x":10,"y":20}';
-const coordiante:{x:number,y:number} = JSON.parse(json);
+const coordiante: { x: number; y: number } = JSON.parse(json);
 console.log(coordiante);
 
 //2)when we declare a variable on one line and initalize it later
 
-let words = ["blue","red"];
-let findWord : boolean;
-for(let i = 0;i<words.length;i++){
-  if(words[i] === "blue"){
+let words = ["blue", "red"];
+let findWord: boolean;
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "blue") {
     findWord = true;
   }
 }
 
 //3) variable whose type cannot be inferred correctly
 
-let numbers = [3,-1,4];
-let numberAboveZero : boolean | number = false;
+let numbers = [3, -1, 4];
+let numberAboveZero: boolean | number = false;
 
-for(let i = 0;i<numbers.length;i++){
-  if(numbers[i] > 0){
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
     numberAboveZero = numbers[i];
   }
 }
